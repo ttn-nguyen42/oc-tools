ed385debbc9b7d5caf006c5d94ebf37e327e74cb
Nguyen Tran <ttn.nguyen42@gmail.com>
2022-12-23 10:24:10 +0700

    updated createUsb.sh

--- Utilities/macrecovery/Monterey/getMonterey.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Monterey
-python3 ../macrecovery.py -b Mac-E43C1C25D4880AD6 -m 00000000000000000 download
--- createUsb.sh
+++ createUsb.sh
@@ -1,11 +1,79 @@
 #!/bin/bash
 
-# USB EFI directory
-usb_efi_dir="$1"
-64_dir="$1/X64"
-efi_dir="$1/x64/EFI"
+# Root directory
+root_dir=$(pwd)
 
-# Location of MacOS packages
-macos_pkg_dir="$2"
-basesystem_chunklist="$2/BaseSystem.chunklist"
-basesystem_dmg="$2/BaseSystem.dmg"
+# USB EFI directory
+x64_dir="$root_dir/X64"
+efi_dir="$x64_dir/EFI"
+
+# Location of the MacOS packages installers
+macos_pkg_dir="$root_dir/Utilities/macrecovery"
+
+get_package() {
+    if [ "$1" == "sur" ]
+    then
+        echo "Downloading Big Sur packages"
+        # Big Sur
+        python3 $macos_pkg_dir/macrecovery.py -b Mac-42FD25EABCABB274 -m 00000000000000000 download
+
+    elif [ "$1" == "monterey" ]
+    then
+        echo "Downloading Monterey packages"
+        # Monterey
+        python3 $macos_pkg_dir/macrecovery.py -b Mac-42FD25EABCABB274 -m 00000000000000000 download
+    else
+        echo "Recovery image not specified"
+        echo "Options: sur | monterey"
+        exit 10
+    fi
+    # Move package to root directory
+    mkdir -p $root_dir/recovery_$1
+
+    mv $root_dir/com.apple.recovery.boot $root_dir/recovery_$1
+    echo "Recovery image downloaded"
+}
+
+add_efi() {
+    if [ -z "$1" ]; then
+        echo "Missing the path for USB drive letter"
+        exit 10
+    fi
+    usb_dir="/mnt/$1"
+}
+
+open_plist() {
+    if [ -z "$1" ]; then
+        echo "Missing the path for config.plist"
+    fi
+}
+
+get_tools() {
+    echo "Unimplemented"
+}
+
+# Options
+case "$1" in
+get-package)
+    get_package "$2"
+    ;;
+add-efi)
+    add_efi "$2"
+    ;;
+plist)
+    open_plist "$2"
+    ;;
+get-tools)
+    get_tools
+    ;;
+*)
+    echo "Help"
+    echo "$0 get-package [sur | monterey]"
+    echo "$0 get-tools"
+    echo "$0 add-efi [path]"
+    echo "$0 open-plist [path]"
+    exit 10
+    ;;
+esac
+
+exit $?
